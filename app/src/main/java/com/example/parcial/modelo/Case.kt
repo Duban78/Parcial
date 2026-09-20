134ab7b6cf55055d2ca64897aece009a54957d72
Duban78 <dubanayala31@gmail.com>
2026-09-19 22:03:40 -0500

    Model and case added

---
+++ app/src/main/java/com/example/parcial/modelo/Case.kt
@@ -0,0 +1,10 @@
+package com.example.parcial.model
+
+data class Caso(
+    val id: String,
+    val titulo: String,
+    val descripcion: String,
+    val cliente: String,
+    val fechaInicio: String,
+    val estado: String // "En investigación" o "Cerrado"
+)
